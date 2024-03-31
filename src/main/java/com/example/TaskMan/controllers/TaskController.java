d2e65d3828ee8fd4823da75be31ca4616148fbf1
Szymon Olkiewicz <szymon.olkiewicz@tronik.pl>
2024-03-31 18:49:53 +0200

    Controller

--- src/main/java/com/example/TaskMan/controllers/TaskController.java
+++ src/main/java/com/example/TaskMan/controllers/TaskController.java
@@ -1,7 +1,4 @@
 package com.example.TaskMan.controllers;
-
-import com.example.TaskMan.services.TaskService;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
